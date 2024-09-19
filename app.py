7ce149aa2a5335e33b2ef7dbc31e02b3f6b1a9f7
Vickotoria8 <vikastar810@gmail.com>
2024-09-19 07:56:05 +0300

    В app.py код для работы с бд

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from app.User._init_ import register_user_module
 from app.Organization._init_ import register_organizer_module
 from app.Depth._init_ import register_department_employee_module
 
+from app import db
+
+
 def create_app():
     app = Flask(__name__)
     # Подключение модулей
@@ -21,3 +24,83 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+# Настройка подключения к базе данных
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Определение модели для таблицы users
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    userID = db.Column(db.BigInteger, primary_key=True)
+    userFirstName = db.Column(db.String)
+    userSecondName = db.Column(db.String)
+    userSurName = db.Column(db.String)
+    roleID = db.Column(db.Integer)
+    email = db.Column(db.String(255))
+    Town = db.Column(db.String)
+    District = db.Column(db.String)
+
+# Определение модели для таблицы DepartmentMembers
+class DepartmentMembers(db.Model):
+    __tablename__ = 'DepartmentMemebers'
+    
+    DepartmentMemberID = db.Column(db.BigInteger, primary_key=True)
+    userID = db.Column(db.BigInteger)
+
+# Определение модели для таблицы EventOrganizers
+class EventOrganizer(db.Model):
+    __tablename__ = 'EventOrganizers'
+    
+    organizerID = db.Column(db.BigInteger, primary_key=True)
+    userID = db.Column(db.BigInteger)
+
+# Определение модели для таблицы Events
+class Event(db.Model):
+    __tablename__ = 'Events'
+    
+    eventID = db.Column(db.BigInteger, primary_key=True)
+    Rate = db.Column(db.ARRAY(db.Numeric))
+    Category = db.Column(db.String)
+    StartDate = db.Column(db.Date)
+    FinalDate = db.Column(db.Date)
+    organizerID = db.Column(db.BigInteger)
+
+# Определение модели для таблицы PersonOnEvent
+class PersonOnEvent(db.Model):
+    __tablename__ = 'PersonOnEvent'
+    
+    eventID = db.Column(db.BigInteger, nullable=False)
+    userID = db.Column(db.BigInteger, nullable=False)
+    
+    __table_args__ = (
+        db.PrimaryKeyConstraint('eventID', 'userID'),
+    )
+
+
+
+@app.route('/')
+def index():
+    return "Welcome to the Events API"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+db.create_all()
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    new_user = User(userFirstName='Иван', userSecondName='Иванов', userSurName='Иванович', roleID=1, email='ivan@example.com')
+    db.session.add(new_user)
+    db.session.commit()
+    return "User added"
